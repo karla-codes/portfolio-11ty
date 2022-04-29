961ea22e4936954bcfb5b3a1e56e449009cdef6e
karla-walker <indiablonix@gmail.com>
2022-04-29 04:19:11 -0700

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = eleventyConfig => {
+  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy("./src/css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};

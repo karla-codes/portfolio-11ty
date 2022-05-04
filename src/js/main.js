3a4f10db16376ae9c29ac626d74c72a6fcf57eca
karla-walker <indiablonix@gmail.com>
2022-05-03 21:15:20 -0700

    Add js file for button toggle

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("Hello there!");
+
+const btns = document.querySelectorAll(".button");
+
+// loop through buttons and add click event listener
+btns.forEach(btn => {
+  // for each button:
+  // - find content section and store
+  // - toggle 'display: hidden' from content section when clicked
+  const contentSection = btn.parentElement.nextElementSibling;
+  btn.addEventListener("click", () => {
+    contentSection.classList.toggle("display-toggle");
+    console.log("I was clicked");
+  });
+});

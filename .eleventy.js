c7a1d4ad9e451e6225ae781ff6e884d4e18fb312
karla-walker <indiablonix@gmail.com>
2022-06-22 03:51:04 -0700

    Add passthroguh copy of for fonts folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/fonts");
 
   return {
     dir: {

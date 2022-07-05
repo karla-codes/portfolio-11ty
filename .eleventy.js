734237889cf8d45b413298ae83521ba9f9bc3f79
karla-walker <indiablonix@gmail.com>
2022-07-04 23:39:46 -0700

    Make Nunjucks the templating language

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,16 @@
 module.exports = eleventyConfig => {
-  eleventyConfig.addPassthroughCopy("./src/js");
-  eleventyConfig.addWatchTarget("./src/sass/");
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/fonts");
+  eleventyConfig.addPassthroughCopy("./src/js")
+  eleventyConfig.addWatchTarget("./src/sass/")
+  eleventyConfig.addPassthroughCopy("./src/css")
+  eleventyConfig.addPassthroughCopy("./src/fonts")
 
   return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
       input: "src",
       output: "public",
     },
-  };
-};
+  }
+}

573316858cf7e29d585e979f95a94b2df99e1cdc
karla-walker <indiablonix@gmail.com>
2022-07-22 14:27:46 -0700

    Add todo

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,19 @@
-"use strict";
+"use strict"
 
-console.log("Hello there!");
+// TODO Check if I need this JS before making live
 
-const btns = document.querySelectorAll(".button");
+console.log("Hello there!")
+
+const btns = document.querySelectorAll(".button")
 
 // loop through buttons and add click event listener
 btns.forEach(btn => {
   // for each button:
   // - find content section and store
   // - toggle 'display: hidden' from content section when clicked
-  const contentSection = btn.parentElement.nextElementSibling;
+  const contentSection = btn.parentElement.nextElementSibling
   btn.addEventListener("click", () => {
-    contentSection.classList.toggle("display-toggle");
-    console.log("I was clicked");
-  });
-});
+    contentSection.classList.toggle("display-toggle")
+    console.log("I was clicked")
+  })
+})
